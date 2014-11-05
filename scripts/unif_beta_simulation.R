7ffc1108ec5fe475acb9a5dfec0e3541ea8fca09
you1025 <you1025@gmail.com>
2014-11-05 22:39:01 +0900

    コメントとか入れてみた

--- scripts/unif_beta_simulation.R
+++ scripts/unif_beta_simulation.R
@@ -1,4 +1,11 @@
+# 検証:
+# [0-1] の一様分布に従う p+q-1 個の確率変数を大きさの順で並び替えると
+# 小さい方から p 番目の値は β(p, q) に従う
+
+# サンプリング回数
 n <- 100000
+
+# β分布のパラメータ
 p <- 9
 q <- 2
 
@@ -7,5 +14,6 @@ for(i in 1:n) {
   m.values[i,] <- sort(runif(p+q-1, 0, 1), decreasing=F)
 }
 
+# 結果の図示
 hist(m.values[, p], breaks=seq(0, 1, 0.01), freq=F)
 lines(x=seq(0, 1, 0.01), y=dbeta(x=seq(0, 1, 0.01), p, q), col="red")
